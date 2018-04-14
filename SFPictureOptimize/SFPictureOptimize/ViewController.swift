ce7e4def14225c5db8dde3985ab068e2b593fa84
lushengfu <cylushengfu@163.com>
2018-04-14 14:40:13 +0800

    测试GPU切圆角,及生成图片

--- SFPictureOptimize/SFPictureOptimize/ViewController.swift
+++ SFPictureOptimize/SFPictureOptimize/ViewController.swift
@@ -12,14 +12,59 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let rect = CGRect(x: 0, y: 0, width: 160, height: 160)
+        
+        let imgView = UIImageView(frame: rect)
+        
+        imgView.center = view.center
+        
+        view.addSubview(imgView)
+        
+        let image = #imageLiteral(resourceName: "avatar_default_big")
+        
+        
+        imgView.image =  clipCircleAvatarImage(image: #imageLiteral(resourceName: "avatar_default_big"), size: image.size)
+        
+//        imgView.backgroundColor = UIColor.red
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    private func avatarImage(image: UIImage, size: CGSize) -> UIImage? {
+        
+        let rect = CGRect(origin: CGPoint(), size: size)
+        // 获取图片的上下文
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        image.draw(in: rect)
+        
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return result
+        
     }
-
+    
+    // 切圆角图片
+    private func clipCircleAvatarImage(image: UIImage, size: CGSize) -> UIImage? {
+        
+        let rect = CGRect(origin: CGPoint(), size: size)
+        // 获取图片的上下文
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        let path = UIBezierPath(ovalIn: rect)
+        path.addClip()
+        
+        image.draw(at: CGPoint())
+        
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return result
+        
+    }
 
 }
 
