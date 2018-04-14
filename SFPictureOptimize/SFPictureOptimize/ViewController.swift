a2293ccfaf3eacbf017e39bfff9b8a4e4cb9a301
lushengfu <cylushengfu@163.com>
2018-04-14 13:59:09 +0800

    初始化项目

---
+++ SFPictureOptimize/SFPictureOptimize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SFPictureOptimize
+//
+//  Created by happy on 2018/4/14.
+//  Copyright © 2018年 happy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
